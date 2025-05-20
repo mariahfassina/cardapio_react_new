67c96ff3f44d16ecb2b98f7022e1952cea86ca09
mariahfassina <20233017592@estudantes.ifpr.edu.br>
2025-05-20 09:02:43 -0300

    Add files via upload

---
+++ build/precache-manifest.d5bc68e78b549391174c4fc38d301d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcd8d506e6fbc5251162befa2503812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "686b1726bf401ba85a15",
+    "url": "/static/css/main.489aeef5.chunk.css"
+  },
+  {
+    "revision": "aeb870783bd07475b125",
+    "url": "/static/js/2.ab60f1dd.chunk.js"
+  },
+  {
+    "revision": "7e3ab1b9f3c152d87605",
+    "url": "/static/js/3.b3e35730.chunk.js"
+  },
+  {
+    "revision": "686b1726bf401ba85a15",
+    "url": "/static/js/main.bac33841.chunk.js"
+  },
+  {
+    "revision": "dacec0d6d4a94a287a77",
+    "url": "/static/js/runtime~main.61451f2f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28c9c37881d6a75a40ece72dc688665f.js"
+  "/precache-manifest.d5bc68e78b549391174c4fc38d301d6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
